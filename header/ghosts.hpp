95e1e71df80aaf2846b7e836e94ac990c3d1ef07
mohammad <m.karimi.pro99@gmail.com>
2022-07-17 19:56:28 +0430

    add header ghosts

---
+++ header/ghosts.hpp
@@ -0,0 +1,21 @@
+#ifndef _GHOSTS
+#define _GHOSTS
+#include "ghosts_state.hpp"
+
+#define LP 7
+// typedef long long int llint;
+
+class Ghosts
+{
+private:
+    int X; // موقعیت طولی
+    int Y; // موقعیت عرضی
+    ghosts_state * current_state;
+public:
+    void set_xy(int, int);
+    int get_x() const;
+    int get_y() const;
+   
+};
+
+#endif
