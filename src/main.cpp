a80737b154e243ead92e3755f0272111dcc0c547
mohammad <m.karimi.pro99@gmail.com>
2022-07-17 23:16:18 +0430

    add header to main

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
+#include <string>
+#include <vector>
+#include <stdexcept>
+#include <iomanip>
+#include "includefile.hpp"
 
 using namespace std;
 
 int main()
 {
+    PacMan m;
     char ch;
     cout << "Enter a number : ";
     ch = cin.get();
