76eb407f434141ae400cf9f217cb1138df6c2c49
mohammad <m.karimi.pro99@gmail.com>
2022-07-18 03:57:45 +0430

    changes just for test code

--- src/main.cpp
+++ src/main.cpp
@@ -3,17 +3,26 @@
 #include <vector>
 #include <stdexcept>
 #include <iomanip>
-#include "includefile.hpp"
-
+//#include "includefile.hpp"
+#include "../header/PacMan.hpp"
 using namespace std;
 
 int main()
 {
-    while (1)
-    {
-        PacMan(1 , 2);
-    }
-    
-    
+    PacMan p;
+
+    cout << "Life: " << p.get_life() << endl
+         << "Score: " << p.get_score() << endl
+         << "High score: " << p.get_HighScore() << endl
+         << "Position ----- X: " << p.get_x() << "--- Y: " << p.get_y() << endl; 
+    cout << "..................................................." << endl;
+    p.set_score(5555);
+    p.update_HighScore();
+    cout << "Life: " << p.get_life() << endl
+         << "Score: " << p.get_score() << endl
+         << "High score: " << p.get_HighScore() << endl
+         << "Position ----- X: " << p.get_x() << "--- Y: " << p.get_y() << endl; 
+
+    cout << "End...." << endl;
     return 0;
 }
