ddc3b49ff97adb31e95616eed65e1a5b388cdaa6
mohammad <m.karimi.pro99@gmail.com>
2022-08-07 10:39:18 +0430

    Create Header file *Informaion* در این هدر فیل متغیر های اساسی برنامه را مقدار دهی و نگهداری میکنیم متغیر هایی اساسی هستند که در طول برنامه استفاده ای ثابت و بلند مدت دارند

---
+++ header/Information.hpp
@@ -0,0 +1,7 @@
+// In this file, we put the information of different variables and constants of the program
+/*
+سلول ها اساسا در کلاس نقشه استفاده میشوند 
+
+*/
+unsigned int Cell_width = 12;
+unsigned int Cell_Height = 12;
