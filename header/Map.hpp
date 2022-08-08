f317e1612c55203a2165e9000a4229b3a772c449
mohammad <m.karimi.pro99@gmail.com>
2022-08-08 05:43:36 +0430

    Add function DrowMap... وظیفه این تابع رسم نقشه بازی است

--- header/Map.hpp
+++ header/Map.hpp
@@ -1,12 +1,15 @@
-#pragma once
+#ifndef MAP_HPP
+#define MAP_HPP
+
+
 #include <array>
 #include <string>
-
-#include "Information.hpp"
+#include <SFML/Graphics.hpp>
+//#include "Information.hpp"
 
 #define Cell_Height 21 // ارتفاع نقشه  بدون ضریب اندازه هر سلول
 #define Cell_Weight 21 // عرض نقشه
-
+#define Cell_Size 30 // اندازه هر سلول با واحد پیکسل
 
 class Map
 {
@@ -18,10 +21,8 @@ class Map
         Map();
         void Set_Pattern();
         void ToEqualize();
-        void DrowMap();
-        
-
+        void DrowMap(sf::RenderWindow & );
 };
 
 
-    
+#endif
--- src/Map.cpp
+++ src/Map.cpp
@@ -1,4 +1,7 @@
 #include "Map.hpp"
+#include <iostream>
+
+
 Map::Map()
 {
 	Set_Pattern();
@@ -47,18 +50,65 @@ void Map::ToEqualize()
 			switch (PatternMap[row][col])
 			{
 				case '#' :
-					MainMap[row][col] = Cell::Wall;
+					MainMap[col][row] = Cell::Wall;
 					break;
 				case '.' :
-					MainMap[row][col] = Cell::Pellet;
+					MainMap[col][row] = Cell::Pellet;
 					break;
 				case '*' :
-					MainMap[row][col] = Cell::Power_Pellet;
+					MainMap[col][row] = Cell::Power_Pellet;
+					break;
+				case '=' :
+					MainMap[col][row] = Cell::Door;
 					break;
 				default :
-					MainMap[row][col] = Cell::Empty;
+					MainMap[col][row] = Cell::Empty;
 					break;
 			}// End Switch-Case
 		}// End for 2
 	}// End for 1
 }// End function ToEqualize
+
+void Map::DrowMap(sf::RenderWindow & win)
+{
+	sf::RectangleShape S_wall(sf::Vector2f(Cell_Size, Cell_Size));
+	S_wall.setFillColor(sf::Color::Blue);
+
+	sf::CircleShape S_pellete(2);
+	S_pellete.setFillColor(sf::Color::Yellow);
+
+	sf::CircleShape S_power_pellete(6);
+	S_power_pellete.setFillColor(sf::Color::Yellow);
+
+	sf::RectangleShape S_door(sf::Vector2f(Cell_Size, Cell_Size/2));
+	S_door.setFillColor(sf::Color::Magenta);
+
+	for (int row = 0; row < Cell_Height ; row++){
+		for (int col = 0; col < Cell_Weight; col++){
+
+			if (MainMap[row][col] == Cell::Wall)
+			{
+				S_wall.setPosition(sf::Vector2f(row * Cell_Size, col * Cell_Size));
+				win.draw(S_wall);
+			}
+			if (MainMap[row][col] == Cell::Pellet)
+			{
+				S_pellete.setPosition
+				(sf::Vector2f(row * Cell_Size + (Cell_Size/2), col * Cell_Size + (Cell_Size/2)));
+				win.draw(S_pellete);
+			}
+			if (MainMap[row][col] == Cell::Power_Pellet)
+			{
+				S_power_pellete.setPosition
+				(sf::Vector2f(row * Cell_Size + (Cell_Size/2 - 3), col * Cell_Size + (Cell_Size/2 -3 )));
+				win.draw(S_power_pellete);
+			}
+			if (MainMap[row][col] == Cell::Door)
+			{
+				S_door.setPosition(sf::Vector2f(row * Cell_Size, col * Cell_Size));
+				win.draw(S_door);
+			}
+
+		}// End for 2
+	}// End for 1
+}
