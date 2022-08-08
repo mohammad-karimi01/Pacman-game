669ae8c707b18c4ae55e2014f3df91b235c754e4
mohammad <m.karimi.pro99@gmail.com>
2022-08-08 05:51:06 +0430

    Dellete this files به علت تغییر در منطق بازی دیگر این کلاس ها نیاز نبودند در نتیجه حذف شدند

--- src/pellets.cpp
+++
@@ -1,9 +0,0 @@
-#include "pellets.hpp"
-#include <iostream>
-
-using namespace std;
-
-void pellets::print()
-{
-    cout << "pellets" << endl;
-}// End function*
--- src/power_pellets.cpp
+++
@@ -1,9 +0,0 @@
-#include "power_pellets.hpp"
-#include <iostream>
-
-using namespace std;
-
-void power_pellets::print()
-{
-    cout << "power_pellets" << endl;
-}// End function*
