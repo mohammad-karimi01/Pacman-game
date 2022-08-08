5c2e35ecb136d425ddf82ea591905828d3afcc04
mohammad <m.karimi.pro99@gmail.com>
2022-08-08 15:00:21 +0430

    Transferring values ​​from the header Map

--- header/Information.hpp
+++ header/Information.hpp
@@ -7,3 +7,6 @@
 #define Cell_Height 21 // ارتفاع نقشه  بدون ضریب اندازه هر سلول
 #define Cell_Weight 21 // عرض نقشه
 #define Cell_Size 30 // اندازه هر سلول با واحد پیکسل
+#define MAX_SPEED 4 // بیشینه سرعت پک من و ارواح
+
+int Level = 1;
