c19b5593f87df9473bc218c26649ebe46e03e384
mohammad <m.karimi.pro99@gmail.com>
2022-08-08 14:58:03 +0430

    Transferring values ​​to the header Information

--- header/Map.hpp
+++ header/Map.hpp
@@ -6,22 +6,20 @@
 #include <string>
 #include <SFML/Graphics.hpp>
 //#include "Information.hpp"
-
-#define Cell_Height 21 // ارتفاع نقشه  بدون ضریب اندازه هر سلول
-#define Cell_Weight 21 // عرض نقشه
-#define Cell_Size 30 // اندازه هر سلول با واحد پیکسل
-
+#include "Information.hpp"
 class Map
 {
     private :
         enum Cell {Wall, Empty, Pellet, Power_Pellet, Door};
         std::array<std::string, Cell_Height> PatternMap; 
-        std::array<std::array<Cell,Cell_Height>, Cell_Weight> MainMap;  
+        std::array<std::array<Cell,Cell_Height>, Cell_Weight> MainMap; 
+         
     public :
         Map();
         void Set_Pattern();
         void ToEqualize();
         void DrowMap(sf::RenderWindow & );
+        void UpdateMap();
 };
 
 
