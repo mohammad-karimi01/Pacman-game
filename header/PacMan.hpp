b87e5015aeecfed82e51bebf068ed36b9011b515
mohammad <m.karimi.pro99@gmail.com>
2022-08-09 17:24:46 +0430

    improvement and Further completion of the class

--- header/PacMan.hpp
+++ header/PacMan.hpp
@@ -1,37 +1,53 @@
 #ifndef _PACMAN
 #define _PACMAN
 #include "Information.hpp"
+#include "Snacks.hpp"
+#include <SFML/Graphics.hpp>
+#include "Map.hpp"
+#include <array>
 typedef long long int llint;
 
+enum Directions { Stop, Right, Left, Up, Down};
 
 class PacMan
 {
 private:
-    int X; // موقعیت طولی
-    int Y; // موقعیت عرضی
+    float Pos_X; // موقعیت طولی
+    float Pos_Y; // موقعیت عرضی
     int Life; // تعداد جان های پک من
     llint Score; // مقدار امتیاز پک من  
     llint HighScore;
     float CurrentSpeed;
+    Directions direction;
+
+    sf::Sprite PacmanShape;
+
+
     ///////////////////
     void set_HighScore();
+    void Set_Speed(int level);
+    bool collison(const std::array<std::array<Cell,Cell_Height>, Cell_Weight> &, Directions D);
 public:
     //PacMan(int , int);
     PacMan();
     ~PacMan();
     
-    void Set_Speed(int level);
     
+    void Update(const std::array<std::array<Cell,Cell_Height>, Cell_Weight> &);
+    void Set_X(float );
+    void Set_Y(float );
+    void Move(int ); // parameter is Game level
+    
+
     int get_life() const;
-    void set_xy(int, int);
-    int get_x() const;
-    int get_y() const;
-    void set_score(llint );
+    void set_score(SnackType , int = 0);
     llint get_score() const;
     llint get_HighScore() const;
     
     void update_HighScore();
     void Destroy();
+
+    void Drow(sf::RenderWindow & );
 };
 
 #endif
--- src/PacMan.cpp
+++ src/PacMan.cpp
@@ -1,16 +1,18 @@
-#include "../header/PacMan.hpp"
+#include "PacMan.hpp"
+
 #include <stdexcept>
 #include <fstream>
 #include <iostream>
+#include <cmath>
 #define POS_X 10 // موقعیت پیش فرض برای محل ایجاد پک من
 #define POS_Y 10
 
 using namespace std;
 
  
- 
+ /*
 
-PacMan::PacMan() 
+PacMan::PacMan(): Score(0)
 {
     set_xy(POS_X, POS_Y); //سازنده پیش فرض برای مقدار دهی موقعیت اولیه پک من هنگام تولد برای شرع بازی
     set_HighScore();
@@ -23,104 +25,48 @@ PacMan::~PacMan()
 {
     cout << "Distructor...." << endl;
 }
+*/
+
+void PacMan::Set_X(float x)
+{
+
+}
+void PacMan::Set_Y(float y)
+{
+
+}
 
 void PacMan::Set_Speed(int level)
 {
     float MaxSpeed = static_cast<float>(MAX_SPEED);
     
     if (level >= 1 && level <= 4){
-        CurrentSpeed = MaxSpeed * (80/100); 
+        CurrentSpeed = static_cast<float>(MaxSpeed) * (80/100); 
     }
     else if (level >= 5 && level <= 20){
-        CurrentSpeed = MaxSpeed * (90/100); 
+        CurrentSpeed = static_cast<float>(MaxSpeed) * (90/100); 
     }
     else if (level >= 21 && level <= 32){
-        CurrentSpeed = MaxSpeed ; 
+        CurrentSpeed = static_cast<float>(MaxSpeed) ; 
     }
     else if (level >= 33){
-        CurrentSpeed = MaxSpeed * (90/100); 
+        CurrentSpeed = static_cast<float>(MaxSpeed) * (90/100); 
     }
     else{
         throw out_of_range("Error: The value of Levels game out of range(Not be negatives number)");
     }
 }//End function Set_Level
 
-
-
-int PacMan::get_life() const
+/*
+این تابع با استفاده از توابع تعریف شده در هدر 
+snacks.hpp && .cpp
+اقدام به محاسبه ی امتیاز پک من در حین بازی میکند
+*/
+void PacMan::set_score(SnackType type, int num)
 {
-    return Life;
-}//End function*
+    Score += GetScores_Snacks(type, num); // this function define in header snakc.hpp
+}// End function SetScore
 
-// here set the element "X and Y"  of pacman 
-void PacMan::set_xy(int x, int y)
-{
-    this->X = x;
-    this->Y = y;
-}// End function*
-
-int PacMan::get_x() const
-{
-    return X;
-}// End function*
-
-int PacMan::get_y() const
-{
-    return Y;
-}// End function*
-
-// here set the element "Score"  of pacman
-void PacMan::set_score(llint s )
-{
-    Score = s;
-
-}// End function*
-
-llint PacMan::get_score() const
-{
-    return Score;
-}// End function*
-
-void PacMan::set_HighScore()
-{
-    ifstream file("HighScore.txt", ios::in | ios::ate);
-    if (!file){
-        HighScore = 0;
-        update_HighScore();
-        //throw runtime_error("Error: The file \" HighScore \" could not be opened...");
-    }
-    else {
-        file.seekg(0, ios::beg);
-        file >> HighScore;
-        if (HighScore < 0)
-        {
-            throw out_of_range("HighScore is out of range");
-        }
-    }
-    file.close();
-}// End function*
-
-llint PacMan::get_HighScore() const
-{
-    return HighScore;
-
-}// End function*
-
-void PacMan::update_HighScore() 
-{
-    if ( Score >= HighScore)
-    {
-        ofstream file("HighScore.txt", ios::out);
-        if(!file)
-        {
-            throw runtime_error("Error: The file \" HighScore \" could not be opened...");
-        }
-        HighScore = Score;
-
-        file << Score;
-    }
-
-}// End function*
 
 void PacMan::Destroy()
 {
@@ -130,4 +76,116 @@ void PacMan::Destroy()
         cout << "GAME OVER !" << endl;
     }
 
+}// End function Destroy
+
+void PacMan::Drow(sf::RenderWindow &)
+{
+
+}// End function Drow
+
+bool PacMan::collison
+(const std::array<std::array<Cell,Cell_Height>, Cell_Weight> & CurrentMap, Directions D)
+{
+    float tempx = Pos_X / static_cast<float>(Cell_Size);
+    float tempy = Pos_Y / static_cast<float>(Cell_Size);
+    int x, y;
+    if (D == Right){
+        x = static_cast<int>(ceil(tempx));
+        y = static_cast<int>(tempy);
+        if (CurrentMap[x+1][y] == Cell::Wall){
+            Pos_X = (float)x;
+            // Pos_Y not change
+            return true;
+        }
+        
+    }
+    else if (D == Up){
+        x = static_cast<int>(tempx);
+        y = static_cast<int>(floor(tempy));
+        if (CurrentMap[x][y-1] == Cell::Wall){
+            //Pos_X not change
+             Pos_Y = static_cast<float>(y);
+            return true;
+        }
+    }
+    else if (D == Left){
+        x = static_cast<int>(floor(tempx));
+        y = static_cast<int>(tempy);
+        if (CurrentMap[x-1][y] == Cell::Wall){
+            Pos_X = (float)x;
+            // Pos_Y not change
+            return true;
+        }
+    }
+    else if (D == Down){
+        x = static_cast<int>(tempx);
+        y = static_cast<int>(ceil(tempy));
+        if (CurrentMap[x][y+1] == Cell::Wall){
+            //Pos_X not change
+             Pos_Y = static_cast<float>(y);
+            return true;
+        }
+    }
+    return false;
+}// End function Collison
+void PacMan::Update(const std::array<std::array<Cell,Cell_Height>, Cell_Weight> & CurrentMap)
+{
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+	{
+        if (collison(CurrentMap, Directions::Right ))
+            direction = Stop;
+        else
+            direction = Right; 
+	}
+	else if (1 == sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+	{
+		if (collison(CurrentMap, Directions::Up))
+		    direction = Stop;
+        else
+            direction = Up;
+	}
+	else if (1 == sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+	{
+		if (collison(CurrentMap, Directions::Left))
+		    direction = Stop;
+        else
+            direction = Left;
+	}
+	else if (1 == sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
+	{
+		if (collison(CurrentMap, Directions::Down))
+		    direction = Stop;
+        else
+            direction = Down;
+	}
+    else{
+        if (collison(CurrentMap, direction))
+		    direction = Stop;
+        else
+            direction = direction;
+    }
+    
+}// End function Update
+
+void PacMan::Move(int l)
+{
+    Set_Speed(l); // l is level Game
+    if (direction == Right){
+        Pos_X += CurrentSpeed;
+    }
+    else if (direction == Up){
+        Pos_Y -= CurrentSpeed;
+    }
+    else if (direction == Left){
+        Pos_X -= CurrentSpeed;
+    }
+    else if (direction == Down){
+        Pos_Y += CurrentSpeed;
+    }
+    /*
+    else if (direction == Stop){
+        Pos_X and Pos_Y   Not Changel
+    }
+    see function Collison
+    */
 }
