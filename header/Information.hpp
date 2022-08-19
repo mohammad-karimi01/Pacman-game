5455599ca1c48081da8a7d797dfcaf7b4f4962ec
mohammad <m.karimi.pro99@gmail.com>
2022-08-19 07:33:16 +0430

    add define Max speed = 2

--- header/Information.hpp
+++ header/Information.hpp
@@ -8,7 +8,7 @@
 #define Cell_Height 21 // ارتفاع نقشه  بدون ضریب اندازه هر سلول
 #define Cell_Weight 21 // عرض نقشه
 #define Cell_Size 32 // اندازه هر سلول با واحد پیکسل
-#define MAX_SPEED 4 // بیشینه سرعت پک من و ارواح
+#define MAX_SPEED 2 // بیشینه سرعت پک من و ارواح
 
 #define PacManStartGame_X 10
 #define PacManStartGame_Y 15
